6b9b4eb0774e0e5e61d94c0b39c6d5ea9ee171a8
Volker Hartmann <volker.hartmann@kit.edu>
2021-06-22 16:44:31 +0200

    Ignore regexp to uncomment notes.

--- makeRestDocu.sh
+++ makeRestDocu.sh
@@ -2,5 +2,5 @@
 # https://tinyapps.org/blog/201701240700_convert_asciidoc_to_markdown.html
 asciidoc -b docbook restDocu.adoc
 pandoc -f docbook -t markdown_strict restDocu.xml -o restDocu.md
-sed -i "s/>[ ]*//g" restDocu.md
+#sed -i "s/^>\s*\(.*$\)/\1/" restDocu.md
 sed -i "s/\*\([^\*]\+\)\*\([^\*]\)/'\1'\2/g" restDocu.md
